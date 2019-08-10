64d5fe090aa6b98c4c3a33e9817460e81727535d
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2019-08-09 22:35:38 -0500

    diplayed blanks using text content, added and display an drcrementing counter for guess remaining

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,29 @@
-const wordBank = {
-    1: "Jeremy",
-    2: "Matthew",
-    3: "Jones",
-};
+document.addEventListener('DOMContentLoaded', function(){
+const wordBank = ["jeremy", "matthew", "jones"];
 
 //variable
 let wins = 0;
 let loses = 0;
-let remainingGuess = 10;
+let guesses = 10;
 let lettersGuessed = [];
+// let myWord = wordBank[Math.floor(Math.random()*wordBank.length)];
 let myWord = 'jeremy';
 
-//initalize gameboard update currentWord
-
-
-
+//referance variables
+let instructionText = document.getElementById('instructions-text');
+let currentWord = document.getElementById('currentWord');
+let remainingGuess =document.getElementById('remainingGuesses-text');
 
 
 
 //word = jeremy
 if (myWord === 'jeremy'){
     //turn word into a array
-    let myArray = ['_', '_', '_', '_', '_','_'];
+    let myArray = ['\_', '\_', '\_', '\_', '\_','\_'];
+    currentWord.textContent = myArray.join(' ');
+    remainingGuess.textContent = 'Guesses Remaining: ' + guesses; 
+
+
     //Event listener
     document.onkeyup = function (event){
         let userGuess = event.key;
@@ -29,32 +31,35 @@ if (myWord === 'jeremy'){
         if(userGuess === 'j'){
             myArray[0] = 'j';
             
-        };
-        if(userGuess === 'e'){
+        } else if(userGuess === 'e'){
             myArray[1] = 'e';
             myArray[3] = 'e';  
-        };
-
-        if(userGuess === 'r'){
+        } else if(userGuess === 'r'){
             myArray[2] = 'r';   
-        };
-
-        if(userGuess === 'm'){
+        } else if(userGuess === 'm'){
             myArray[4] = 'm';   
-        };
-        if(userGuess === 'y'){
+        }else if(userGuess === 'y'){
             myArray[5] = 'y';   
-        };
-        console.log(myArray);
-    
-    
+        }else{
+            guesses--; //add if for if ! in letterguessed
+        }
 
+        currentWord.textContent = myArray.join(' ');
+        instructionText.style.color= 'white';
+        remainingGuess.textContent = 'Guesses Remaining: ' + guesses; 
+        
+        
     };
 
     
 };
 
+
+    
+
+
 //endgame
 if (remainingGues = 0){
     //end game
 }
+});

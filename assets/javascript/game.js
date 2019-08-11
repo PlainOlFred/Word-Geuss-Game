cb04eb39816822e7f493dfd274e801f8bb4c1dab
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2019-08-10 19:09:38 -0500

    added catetogory of famous directors

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,79 +1,268 @@
 document.addEventListener('DOMContentLoaded', function(){
     
-    const wordBank = ["jeremy", "matthew", "jones"];
+    const wordBank = ['ALLEN', 'CAMERON', 'HITCHCOCK',"SCORESE", 'SPIELBERG','TARANTINO'];
+
     
     //variable
     let wins = 0;
     let loses = 0;
     let guesses = 10;
+
     let lettersGuessed = [];
     let letterBank = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-    let blankWord = '\_'; //to test wins
-    // let myWord = wordBank[Math.floor(Math.random()*wordBank.length)];
-    let myWord = 'jeremy';
     
-    //referance variables
+    let blank = '\_'; //to test wins
+   
+    let myWord = wordBank[Math.floor(Math.random()*wordBank.length)];
+
+    console.log('word to guess ' + myWord);
+    
+    //reference variables
     let instructionText = document.getElementById('instructions-text');
     let currentWord = document.getElementById('currentWord');
     let remainingGuess =document.getElementById('remainingGuesses-text');
+    let lettersUsedText = document.getElementById('lettersUsed');
     let winsText = document.getElementById('wins');
     let losesText = document.getElementById('loses');
-    let lettersUsedText = document.getElementById('lettersUsed');
+   
 
     //display score board
-    winsText.textContent = 'Wins: ';
-    losesText.textContent = 'Loses: ';
+    winsText.textContent = 'Wins: 0';
+    losesText.textContent = 'Loses: 0';
     remainingGuess.textContent = 'Guesses Remaining: ' + guesses;
 
-    
-    
-    
-    //word = jeremy
-    if (myWord === 'jeremy'){
+    //dis]play game board and current word
+    if (myWord === 'ALLEN'){
         //turn word into a array
-        let myArray = ['\_', '\_', '\_', '\_', '\_','\_'];
-        currentWord.textContent = myArray.join(' ');
+        var myArray = ['\_', '\_', '\_', '\_', '\_'];
+        currentWord.textContent = myArray.join(' ');}
+    if (myWord === 'CAMERON'){
+        //turn word into a array
+        var myArray = ['\_', '\_', '\_', '\_', '\_','\_','\_'];
+        currentWord.textContent = myArray.join(' ');}
+    if (myWord === 'HITCHCOCK'){
+        //turn word into a array
+        var myArray = ['\_', '\_', '\_', '\_', '\_','\_','\_','\_','\_'];
+        currentWord.textContent = myArray.join(' ');}
+    if (myWord === 'SCORESE'){
+        //turn word into a array
+        var myArray = ['\_', '\_', '\_', '\_', '\_','\_','\_'];
+        currentWord.textContent = myArray.join(' ');}
+    if (myWord === 'SPIELBERG'){
+        //turn word into a array
+        var myArray = ['\_', '\_', '\_', '\_', '\_','\_','\_','\_','\_'];
+        currentWord.textContent = myArray.join(' ');}
+    if (myWord === 'TARANTINO'){
+        //turn word into a array
+        var myArray = ['\_', '\_', '\_', '\_', '\_', '\_', '\_', '\_', '\_'];
+        currentWord.textContent = myArray.join(' ');}
+    
 
-        
-        
-         
     
-    
-        //Event listener
-        document.onkeyup = function (event){
-            let userGuess = event.key;
-            
-            
-            if(letterBank.includes(userGuess)){
-                if(userGuess === 'j'){
-                    myArray[0] = 'j';
-                    
-                } else if(userGuess === 'e'){
-                    myArray[1] = 'e';
-                    myArray[3] = 'e';  
-                } else if(userGuess === 'r'){
-                    myArray[2] = 'r';   
-                } else if(userGuess === 'm'){
-                    myArray[4] = 'm';   
-                }else if(userGuess === 'y'){
-                    myArray[5] = 'y';   
-                }else{
-                    lettersGuessed.push(userGuess);
-                    guesses--; //add if for if ! in letterguessed
-                    
-                
-                    letterBank.splice(letterBank.indexOf(userGuess),1);
-                    console.log(letterBank)
-                    
-                    
+      //Event listener
+    document.onkeyup = function (event){
+        let userGuess = event.key;
+            //myWord is ALLEN
+            if (myWord === 'ALLEN'){
+                if(letterBank.includes(userGuess)){
+                    if(userGuess === 'a'){
+                        myArray[0] = 'A';
+                        
+                    } else if(userGuess === 'l'){
+                        myArray[1] = 'L';
+                        myArray[2] = 'L';  
+                    } else if(userGuess === 'e'){
+                        myArray[3] = 'E';   
+                    } else if(userGuess === 'n'){
+                        myArray[4] = 'N';   
+                    }else{
+                        lettersGuessed.push(userGuess);
+                        guesses--; //add if for if ! in letterguessed
+                        letterBank.splice(letterBank.indexOf(userGuess),1);// removes letter from letterBank 
+                    }
+                }       
+            };
+            //myWord is CAMERON
+            if (myWord === 'CAMERON'){
+                if(letterBank.includes(userGuess)){
+                    if(userGuess === 'c'){
+                        myArray[0] = 'C';  
+                    } else if(userGuess === 'a'){
+                        myArray[1] = 'A'; 
+                    } else if(userGuess === 'm'){
+                        myArray[2] = 'M';   
+                    } else if(userGuess === 'e'){
+                        myArray[3] = 'E';   
+                    } else if(userGuess === 'r'){
+                        myArray[4] = 'R';   
+                    } else if(userGuess === 'o'){
+                        myArray[5] = 'O';   
+                    } else if(userGuess === 'n'){
+                        myArray[6] = 'N';   
+                    } 
+                    else{
+                        lettersGuessed.push(userGuess);
+                        guesses--; //add if for if ! in letterguessed
+                        letterBank.splice(letterBank.indexOf(userGuess),1);// removes letter from letterBank 
+                    }
                 }
+            };
+            //myWord is HITCHCOCK
+            if (myWord === 'HITCHCOCK'){
+                if(letterBank.includes(userGuess)){
+                    if(userGuess === 'h'){
+                        myArray[0] = 'H';
+                        myArray[4] = 'H';  
+                    } else if(userGuess === 'i'){
+                        myArray[1] = 'I'; 
+                    } else if(userGuess === 't'){
+                        myArray[2] = 'T';   
+                    } else if(userGuess === 'c'){
+                        myArray[3] = 'C';  
+                        myArray[5] = 'C'; 
+                        myArray[7] = 'C';  
+                    } else if(userGuess === 'o'){
+                        myArray[6] = 'O';   
+                    } else if(userGuess === 'k'){
+                        myArray[8] = 'K';   
+                    } 
+                    else{
+                        lettersGuessed.push(userGuess);
+                        guesses--; //add if for if ! in letterguessed
+                        letterBank.splice(letterBank.indexOf(userGuess),1);// removes letter from letterBank 
+                    }
+                }
+            };
+            //myWord is SCORESE
+            if (myWord === 'SCORESE'){
+                if(letterBank.includes(userGuess)){
+                    if(userGuess === 's'){
+                        myArray[0] = 'S';  
+                        myArray[5] = 'S';
+                    } else if(userGuess === 'c'){
+                        myArray[1] = 'C'; 
+                    } else if(userGuess === 'o'){
+                        myArray[2] = 'O';   
+                    } else if(userGuess === 'r'){
+                        myArray[3] = 'R';  
+                    } else if(userGuess === 'e'){
+                        myArray[4] = 'E'; 
+                        myArray[6] = 'E';   
+                    } else if(userGuess === 'o'){
+                          
+                    } 
+                    else{
+                        lettersGuessed.push(userGuess);
+                        guesses--; //add if for if ! in letterguessed
+                        letterBank.splice(letterBank.indexOf(userGuess),1);// removes letter from letterBank 
+                    }
+                }
+            };
+            //myWord is SPIELBERG
+            if (myWord === 'SPIELBERG'){
+                if(letterBank.includes(userGuess)){
+                    if(userGuess === 's'){
+                        myArray[0] = 'S';  
+                    } else if(userGuess === 'p'){
+                        myArray[1] = 'P'; 
+                    } else if(userGuess === 'i'){
+                        myArray[2] = 'M';   
+                    } else if(userGuess === 'e'){
+                        myArray[3] = 'E'; 
+                        myArray[6] = 'E';  
+                    } else if(userGuess === 'l'){
+                        myArray[4] = 'L';   
+                    } else if(userGuess === 'b'){
+                        myArray[5] = 'B';   
+                    } else if(userGuess === 'r'){
+                        myArray[6] = 'R';   
+                    } else if(userGuess === 'r'){
+                        myArray[7] = 'G';   
+                    } 
+                    else{
+                        lettersGuessed.push(userGuess);
+                        guesses--; //add if for if ! in letterguessed
+                        letterBank.splice(letterBank.indexOf(userGuess),1);// removes letter from letterBank 
+                    }
+                }
+            };
+            // myWord is TARANTINO
+            if (myWord === 'TARANTINO'){
+                if(letterBank.includes(userGuess)){
+                    if(userGuess === 't'){
+                        myArray[0] = 'T'; 
+                        myArray[5] = 'T'; 
+                    } else if(userGuess === 'a'){
+                        myArray[1] = 'A'; 
+                        myArray[3] = 'A'; 
+                    } else if(userGuess === 'r'){
+                        myArray[2] = 'R';   
+                    } else if(userGuess === 'n'){
+                        myArray[4] = 'N';  
+                        myArray[7] = 'N'; 
+                    } else if(userGuess === 'i'){
+                        myArray[6] = 'I';   
+                    } else if(userGuess === 'o'){
+                        myArray[8] = 'O';   
+                    } 
+                    else{
+                        lettersGuessed.push(userGuess);
+                        guesses--; //add if for if ! in letterguessed
+                        letterBank.splice(letterBank.indexOf(userGuess),1);// removes letter from letterBank 
+                    }
+                }
+            };
+        
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+            //endgame
+            if(guesses<1 || !myArray.includes(blank)){
+                //check for loses
+            if(guesses<1 && myArray.includes(blank)){
+                console.log('you lose');
+                //update game board
+                loses ++;
+                losesText.textContent = 'Loses: ' + loses;
+                
+                //restart game 
+                guesses = 10;
+                lettersGuessed = [];
+                letterBank = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+            
+                //new random word
+                myWord = wordBank[Math.floor(Math.random()*wordBank.length)];
+            } else
+            //check for wins
+            if(!myArray.includes(blank)){
+                console.log('you Win');
+                //update game board
+                wins ++;
+                winsText.textContent = 'Wins: ' + wins;
+                
+                //restart game 
+                guesses = 10;
+                lettersGuessed = [];
+                letterBank = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+            
+                //remove correct work new random word
+                myWord = wordBank[Math.floor(Math.random()*wordBank.length)];
+            };
 
             }
-                
-        
-
-
 
             currentWord.textContent = myArray.join(' ');
             instructionText.style.color= 'white';
@@ -81,23 +270,27 @@ document.addEventListener('DOMContentLoaded', function(){
             winsText.textContent = 'Wins: ' + wins;
             losesText.textContent = 'Loses: ' + loses;
             lettersUsedText.textContent = lettersGuessed.join(' ');
-            
+          
 
             
             
-        };
+        
 
 
     
         
+    
+    
+    
+        
+    
+    
+    // endgame
+    // if (remainingGues = 0){
+    //     end game
+    // }
+
+
+    
     };
-    
-    
-        
-    
-    
-    //endgame
-    if (remainingGues = 0){
-        //end game
-    }
-    });
+});

ce19df05fd3e22975e0b0f81d753acab9c9f3b67
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2020-04-19 10:50:49 -0500

    game logic to show wins and loses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,133 +1,175 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener("DOMContentLoaded", function () {
+  //Global and reference variables
+  let currentWord = document.getElementById("currentWord"),
+    instructionText = document.getElementById("instructions-text"),
+    remainingGuess = document.getElementById("remainingGuesses-text"),
+    lettersUsedText = document.getElementById("lettersUsed"),
+    winsText = document.getElementById("wins");
+  losesText = document.getElementById("loses");
 
-    //Global and reference variables
-    let 
-        currentWord = document.getElementById('currentWord'),
-        instructionText = document.getElementById('instructions-text'),
-        remainingGuess = document.getElementById('remainingGuesses-text'),
-        lettersUsedText = document.getElementById('lettersUsed'),
-        winsText = document.getElementById('wins');
-    //Game object
-    let game = {
-        wordBank: ['ALLEN', 'CAMERON', 'HITCHCOCK', 'SCORESE', 'SPIELBERG','TARANTINO'],
-        wins: 0,
-        myWord: null,
-        currentWord: [],
-        remainingGuesses: 0,
-        lettersUsed: [],
+  //Game object
+  const game = {
+    wordBank: {
+      allen: {
+        hint: "Allen hint",
+        poster: "https://via.placeholder.com/150x200",
+      },
+      cameron: {
+        hint: "cameron hint",
+        poster: "https://via.placeholder.com/150x200",
+      },
+      hitchcock: {
+        hint: "hitchcock hint",
+        poster: "https://via.placeholder.com/150x200",
+      },
+      scorese: {
+        hint: "scorese hint",
+        poster: "https://via.placeholder.com/150x200",
+      },
+      spielberg: {
+        hint: "spielberg hint",
+        poster: "https://via.placeholder.com/150x200",
+      },
+      tarantino: {
+        hint: "tarantino hint",
+        poster: "https://via.placeholder.com/150x200",
+      },
+    },
 
-        
+    wins: 0,
+    loses: 0,
+    startingGuesses: 0,
+    remainingGuesses: 0,
+    currentWordText: null,
+    currentWordLetters: [],
+    currentWordLettersGuessed: [],
+    currentWordLettersUsed: [],
 
+    // Game set-up
+    setUp: function () {
+      const myWords = Object.keys(this.wordBank);
 
-        setUp: function() {
-            
-            this.myWord = this.wordBank[Math.floor(Math.random() * this.wordBank.length)];
-            console.log(this.myWord)
+      this.currentWordText =
+        myWords[Math.floor(Math.random() * myWords.length)];
 
-            this.displayBlanks(this.myWord);
-            this.restart();
+      this.currentWordLetters = this.currentWordText.split("");
 
-            this.remainingGuesses = this.myWord.length + 2;
-            remainingGuess.textContent = this.remainingGuesses;
+      this.renderGameLetters();
 
-        },
-       
+      this.startingGuesses = this.currentWordLetters.length - 3;
+      this.remainingGuesses = this.startingGuesses;
+      remainingGuess.textContent = this.remainingGuesses;
+    },
 
-        update: function(guess) {
-            
-            if (this.remainingGuesses > 1) {
-
-                if(this.myWord.includes(guess.toUpperCase())) {
-
-                   
-                    this.showletters(guess.toUpperCase());
-
-
-                } else{
-
-                    this.lettersUsed.push(guess);
-                    lettersUsedText.textContent = this.lettersUsed;
-                    this.remainingGuesses --;
-
-                    remainingGuess.textContent = this.remainingGuesses;
-                }
-            
-            } else {
-                //restart game
-                
-                this.setUp();
-            }
-
-        },
-        
-
-        displayBlanks: function(word) {
-          
-            
-            for(let i =0; i < word.length ; i++){
-                this.currentWord[i] = '\_'
-            }
-            
-            currentWord.textContent= this.currentWord
-        },
-        showletters: function(letter) {
-            
-            for(let blank in this.myWord.split('')) {
-              
-                if (this.myWord.split('')[blank] === letter) {
-                    console.log(letter)
-                    console.log(blank)
-                    this.currentWord[blank] = letter
-
-                }
-                
-       
-            }
-            currentWord.textContent= this.currentWord
-
-        },
-
-        restart: function() {
-            this.lettersUsed = [];
-            lettersUsedText.textContent = this.lettersUsed;
-
-
+    // Game update after letter guess
+    update: function (guess) {
+      if (this.remainingGuesses === 1) {
+        // show answer
+        this.loses++;
+        losesText.innerHTML = this.loses;
+        this.restart();
+      } else {
+        // incorrect guess
+        this.incorrectGuess(guess);
+        // correct guess
+        this.correctGuess(guess);
+        // renderGameLetter
+        this.renderGameLetters();
+        // if win show answer and restart
+        if (this.checkWin() === true) {
+          this.restart();
         }
+      }
+    },
+
+    incorrectGuess: function (guess) {
+      if (
+        this.currentWordLetters.indexOf(guess) === -1 &&
+        this.currentWordLettersUsed.indexOf(guess) === -1
+      ) {
+        this.currentWordLettersUsed.push(guess);
+
+        this.remainingGuesses--;
+        remainingGuess.innerHTML = this.remainingGuesses;
+        lettersUsedText.innerHTML = this.currentWordLettersUsed.join(", ");
+      }
+    },
+
+    correctGuess: function (guess) {
+      // check for solution
+      for (let i = 0; i < this.currentWordLetters.length; i++) {
+        if (
+          guess === this.currentWordLetters[i] &&
+          this.currentWordLettersGuessed.indexOf(guess) === -1
+        ) {
+          this.currentWordLettersGuessed.push(guess);
+        }
+      }
+    },
+
+    // Show Letters or blamks after gu
+    renderGameLetters: function () {
+      let gameLetters = "";
+      for (let i = 0; i < this.currentWordLetters.length; i++) {
+        if (
+          this.currentWordLettersGuessed.indexOf(this.currentWordLetters[i]) !==
+          -1
+        ) {
+          gameLetters += `${this.currentWordLetters[i]} `;
+        } else {
+          gameLetters += "_ ";
+        }
+      }
+      currentWord.innerHTML = gameLetters;
+    },
+
+    checkWin: function () {
+      let didWin = true;
+
+      for (let i = 0; i < this.currentWordLetters.length; i++) {
+        if (
+          this.currentWordLettersGuessed.indexOf(this.currentWordLetters[i]) ===
+          -1
+        ) {
+          didWin = false;
+        }
+      }
+
+      if (didWin) {
+        this.wins++;
+        winsText.innerHTML = this.wins;
+        return didWin;
+      }
+
+      return false;
+    },
+
+    restart: function () {
+      // restart game
+
+      lettersUsedText.innerHTML = "";
+      this.currentWordText = null;
+      this.currentWordLetters = [];
+      this.currentWordLettersGuessed = [];
+      this.currentWordLettersUsed = [];
+      this.startingGuesses = 0;
+      this.remainingGuesses = 0;
+
+      this.setUp();
+      this.renderGameLetters();
+    },
+  };
+
+  // let myWord = this.wordBank[Math.floor(Math.random()*wordBank.length)];
+
+  //call the game
+  game.setUp();
+
+  //Event listener
+  document.onkeyup = function (event) {
+    if (event.keyCode >= 49 && event.keyCode <= 90) {
+      game.guessedLetter = event.key.toLowerCase();
+      game.update(game.guessedLetter);
     }
-
-    
-    // let myWord = this.wordBank[Math.floor(Math.random()*wordBank.length)];
-   
-
-   //call the game
-    game.setUp();
-
-
-    //Event listener
-    document.onkeyup = function (event){
-        guess = event.key.toLowerCase();
-        game.update(guess);          
-    };
-       
-              
-           
-            
-
-
-             
-            
-        
-
-    
+  };
 });
-
-
-
-
-
-
-
-
-
-
-
